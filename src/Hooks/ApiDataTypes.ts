75d212855ed50772deb008a7eb972d8b63e9afa5
imoempire <mrimoempire@gmail.com>
2025-05-22 21:43:50 +0000

    update

---
+++ src/Hooks/ApiDataTypes.ts
@@ -0,0 +1,19 @@
+export interface ProfileResponse {
+  message: string;
+  profile: EmployeeProfile;
+}
+
+interface EmployeeProfile {
+  id: number;
+  employee_id: string;
+  full_name: string;
+  email: string;
+  phone_number: string;
+  start_date: string; // ISO date string (e.g., "2025-05-01")
+  department: string;
+  position: string;
+  technical_skills: string;
+  professional_bio: string;
+  created_at: string; // ISO date-time string (e.g., "2025-05-22T14:05:54.000000Z")
+  updated_at: string; // ISO date-time string (e.g., "2025-05-22T14:05:54.000000Z")
+}
